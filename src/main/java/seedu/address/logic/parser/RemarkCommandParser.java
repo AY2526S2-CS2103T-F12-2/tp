3802ba88843c967a9b8cfa8ea89c0a4eacfc5921
Junwei <zhengjunwei01@gmail.com>
2026-02-20 14:22:52 +0800

    Add remark field for contact info

---
+++ src/main/java/seedu/address/logic/parser/RemarkCommandParser.java
@@ -0,0 +1,17 @@
+public RemarkCommand parse(String args) throws ParseException {
+    requireNonNull(args);
+    ArgumentMultimap argMultimap = ArgumentTokenizer.tokenize(args,
+        PREFIX_REMARK);
+
+    Index index;
+    try {
+        index = ParserUtil.parseIndex(argMultimap.getPreamble());
+    } catch (IllegalValueException ive) {
+        throw new ParseException(String.format(MESSAGE_INVALID_COMMAND_FORMAT,
+            RemarkCommand.MESSAGE_USAGE), ive);
+    }
+
+    String remark = argMultimap.getValue(PREFIX_REMARK).orElse("");
+
+    return new RemarkCommand(index, remark);
+}
