815feff3a1b8f6cabb448f24fe0c25e769a36ef7
Junwei <zhengjunwei01@gmail.com>
2026-03-13 23:30:41 +0800

    add edit button to person card

--- src/main/java/seedu/address/ui/PersonCard.java
+++ src/main/java/seedu/address/ui/PersonCard.java
@@ -1,6 +1,8 @@
 package seedu.address.ui;
 
+import java.time.format.DateTimeFormatter;
 import java.util.Comparator;
+import java.util.function.Consumer;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -28,6 +30,8 @@ public class PersonCard extends UiPart<Region> {
     public final Person person;
 
     private final Runnable onDelete;
+    private final Consumer<String> onEdit;
+    private final int displayedIndex;
 
     @FXML
     private HBox cardPane;
@@ -49,10 +53,12 @@ public class PersonCard extends UiPart<Region> {
     /**
      * Creates a {@code PersonCode} with the given {@code Person} and index to display.
      */
-    public PersonCard(Person person, int displayedIndex, Runnable onDelete) {
+    public PersonCard(Person person, int displayedIndex, Runnable onDelete, Consumer<String> onEdit) {
         super(FXML);
         this.person = person;
         this.onDelete = onDelete;
+        this.onEdit = onEdit;
+        this.displayedIndex = displayedIndex;
         id.setText(displayedIndex + ". ");
         name.setText(person.getName().fullName);
         phone.setText(person.getPhone().value);
@@ -66,5 +72,36 @@ public class PersonCard extends UiPart<Region> {
     @FXML
     private void handleDelete() {
         onDelete.run();
+    }
+
+    @FXML
+    private void handleEdit() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("edit ").append(displayedIndex);
+        sb.append(" n/").append(person.getName().fullName);
+        sb.append(" p/").append(person.getPhone().value);
+        sb.append(" e/").append(person.getEmail().value);
+        sb.append(" a/").append(person.getAddress().value);
+
+        for (var tag : person.getTags()) {
+            sb.append(" t/").append(tag.tagName);
+        }
+        for (var position : person.getPositions()) {
+            sb.append(" po/").append(position.value);
+        }
+        for (var major : person.getMajors()) {
+            sb.append(" m/").append(major.value);
+        }
+        for (var group : person.getGroups()) {
+            sb.append(" g/").append(group.value);
+        }
+
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("HHmm");
+        for (var ah : person.getAvailableHours()) {
+            sb.append(" h/").append(ah.startTime.format(fmt))
+            .append("-").append(ah.endTime.format(fmt));
+        }
+
+        onEdit.accept(sb.toString());
     }
 }
