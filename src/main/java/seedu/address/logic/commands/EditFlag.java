4580efbb7124eb343654da494f03cd1bbfa65007
Harry Lee <harrylee050114@gamil.com>
2026-03-15 00:06:15 +0800

    Update edit logic

--- src/main/java/seedu/address/logic/commands/EditFlag.java
+++ src/main/java/seedu/address/logic/commands/EditFlag.java
@@ -1,7 +1,8 @@
 package seedu.address.logic.commands;
 
 /**
- * The flags used by <code>EditCommand</code>. -r corresponds to reset, -a corresponds to append.
+ * The flags used by <code>EditCommand</code>. -r corresponds to RESET,
+ * -a corresponds to APPEND, no flags corresponds to NONE.
  */
 public enum EditFlag {
     APPEND,
