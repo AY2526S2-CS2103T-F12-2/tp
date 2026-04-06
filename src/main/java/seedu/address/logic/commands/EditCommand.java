d3e80a1b6cdd656f58276787c48ccb6eae68d1a7
Harry Lee <harrylee050114@gamil.com>
2026-04-06 13:03:16 +0800

    Fix previous patch

--- src/main/java/seedu/address/logic/commands/EditCommand.java
+++ src/main/java/seedu/address/logic/commands/EditCommand.java
@@ -93,8 +93,7 @@ public class EditCommand extends Command {
         Person personToEdit = lastShownList.get(index.getZeroBased());
         Person editedPerson = createEditedPerson(personToEdit, editPersonDescriptor);
 
-        System.out.println(personToEdit.isSamePerson(editedPerson));
-        if (!personToEdit.equals(editedPerson) && model.hasPerson(editedPerson)) {
+        if (model.hasPersonExcept(editedPerson, personToEdit)) {
             throw new CommandException(MESSAGE_DUPLICATE_PERSON);
         }
 
